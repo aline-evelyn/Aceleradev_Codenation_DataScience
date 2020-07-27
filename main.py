0dadef3a6654b9bf2bd4f621638183290bb7247b
Aline Evelyn da Silva <56364701+aline-evelyn@users.noreply.github.com>
2020-07-27 18:06:38 -0300

    Desafio semana 7

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Desafio 5
+# # Desafio 6
 # 
-# Neste desafio, vamos praticar sobre redução de dimensionalidade com PCA e seleção de variáveis com RFE. Utilizaremos o _data set_ [Fifa 2019](https://www.kaggle.com/karangadiya/fifa19), contendo originalmente 89 variáveis de mais de 18 mil jogadores do _game_ FIFA 2019.
+# Neste desafio, vamos praticar _feature engineering_, um dos processos mais importantes e trabalhosos de ML. Utilizaremos o _data set_ [Countries of the world](https://www.kaggle.com/fernandol/countries-of-the-world), que contém dados sobre os 227 países do mundo com informações sobre tamanho da população, área, imigração e setores de produção.
 # 
 # > Obs.: Por favor, não modifique o nome das funções de resposta.
 
 # ## _Setup_ geral
 
-# In[48]:
+# In[24]:
 
 
-from math import sqrt
+import math
 
-import pandas as pd
-import matplotlib.pyplot as plt
+import sklearn 
 import numpy as np
-import scipy.stats as sct
+import pandas as pd
 import seaborn as sns
-import statsmodels.api as sm
-import statsmodels.stats as st
-from sklearn.decomposition import PCA
+
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import StandardScaler, KBinsDiscretizer, OneHotEncoder
 
 
-# In[ ]:
+# In[2]:
 
 
 # Algumas configurações para o matplotlib.
@@ -38,43 +40,32 @@ from sklearn.decomposition import PCA
 #sns.set()
 
 
-# In[2]:
-
-
-fifa = pd.read_csv("fifa.csv")
-
-
 # In[3]:
 
 
-fifa.head()
+countries = pd.read_csv("countries.csv")
 
 
 # In[4]:
 
 
-fifa.shape
-
-
-# In[5]:
-
-
-# Excluindo colunas
-columns_to_drop = ["Unnamed: 0", "ID", "Name", "Photo", "Nationality", "Flag",
-                   "Club", "Club Logo", "Value", "Wage", "Special", "Preferred Foot",
-                   "International Reputation", "Weak Foot", "Skill Moves", "Work Rate",
-                   "Body Type", "Real Face", "Position", "Jersey Number", "Joined",
-                   "Loaned From", "Contract Valid Until", "Height", "Weight", "LS",
-                   "ST", "RS", "LW", "LF", "CF", "RF", "RW", "LAM", "CAM", "RAM", "LM",
-                   "LCM", "CM", "RCM", "RM", "LWB", "LDM", "CDM", "RDM", "RWB", "LB", "LCB",
-                   "CB", "RCB", "RB", "Release Clause"
+new_column_names = [
+    "Country", "Region", "Population", "Area", "Pop_density", "Coastline_ratio",
+    "Net_migration", "Infant_mortality", "GDP", "Literacy", "Phones_per_1000",
+    "Arable", "Crops", "Other", "Climate", "Birthrate", "Deathrate", "Agriculture",
+    "Industry", "Service"
 ]
 
-try:
-    fifa.drop(columns_to_drop, axis=1, inplace=True)
-except KeyError:
-    logger.warning(f"Columns already dropped")
+countries.columns = new_column_names
 
+countries.head(5)
+
+
+# ## Observações
+# 
+# Esse _data set_ ainda precisa de alguns ajustes iniciais. Primeiro, note que as variáveis numéricas estão usando vírgula como separador decimal e estão codificadas como strings. Corrija isso antes de continuar: transforme essas variáveis em numéricas adequadamente.
+# 
+# Além disso, as variáveis `Country` e `Region` possuem espaços a mais no começo e no final da string. Você pode utilizar o método `str.strip()` para remover esses espaços.
 
 # ## Inicia sua análise a partir daqui
 
@@ -82,271 +73,281 @@ except KeyError:
 
 
 # Sua análise começa aqui.
-fifa.head()
+countries.dtypes
 
 
 # In[7]:
 
 
-fifa.shape
+#Colando o separador decimal
+countries = pd.read_csv('./countries.csv', decimal = ',')
+countries.head(5)
 
 
-# In[9]:
+# In[8]:
 
 
-fifa.info()
+#Removendo espaçõs nas colunas 'Country' e 'Region'
+countries = countries.apply(lambda x: x.str.rstrip() if x.dtype == 'object' else x)
+countries['Country'][0]
 
-
-# In[10]:
-
-
-fifa_nulos = fifa[fifa.ShotPower.isna()]
-print(fifa_nulos.shape)
-fifa_nulos.info()
-
-
-# In[11]:
-
-
-#criando uma cópia sem os dados nulos
-dados_fifa = fifa.copy()
-
-
-# In[12]:
-
-
-dados_fifa = fifa[fifa.ShotPower.notna()]
-
-
-# In[13]:
-
-
-#confirmando a hipotese
-dados_fifa.isna().sum().sum()
-
-
-# ## Questão 1
-# 
-# Qual fração da variância consegue ser explicada pelo primeiro componente principal de `fifa`? Responda como um único float (entre 0 e 1) arredondado para três casas decimais.
 
 # In[14]:
 
 
-#aplicando PCA usando a base de dados não nulos
-pca = PCA()
-pca.fit(dados_fifa)
+info = pd.DataFrame({'dtype': countries.dtypes,
+                    'unique_vals': countries.nunique(),
+                    'missing%': (countries.isna().sum() / countries.shape[0]) * 100})
 
-
-# In[15]:
-
-
-#retorna a porcentagem da variancia relativa ao primeiro PC
-pca.explained_variance_ratio_[0]
+info.T
 
 
 # In[16]:
 
 
-def q1():
-    return round(float(pca.explained_variance_ratio_[0]), 3)
+#Preenchendo dados ausentes com a média da região
+countries_fill = countries.copy()
 
+# Obtendo nome da coluna
+numeric_cols = countries_fill._get_numeric_data().columns.tolist()
+
+#preenchendo nan com valores médios de groupby por região
+for col in numeric_cols:
+    countries_fill[col] = countries_fill.groupby('Region')[col].apply(lambda x: x.fillna(x.mean()))
+
+countries_fill.head(5)
+
+
+# ## Questão 1
+# 
+# Quais são as regiões (variável `Region`) presentes no _data set_? Retorne uma lista com as regiões únicas do _data set_ com os espaços à frente e atrás da string removidos (mas mantenha pontuação: ponto, hífen etc) e ordenadas em ordem alfabética.
 
 # In[17]:
 
 
-q1()
+def q1():
+    # Retorne aqui o resultado da questão 1.
+    return sorted(countries_fill['Region'].unique())
+
+q1() 
 
 
 # ## Questão 2
 # 
-# Quantos componentes principais precisamos para explicar 95% da variância total? Responda como un único escalar inteiro.
-
-# In[19]:
-
-
-#plotando um gráfico cumulativo para ver a quantidade de features devemos retornar com n_componentes
-plt.figure(figsize=(12,6))
-plt.plot(np.cumsum(pca.explained_variance_ratio_))
-plt.title('Gráfico cumulativo de componentes FIFA 2019')
-plt.xlabel('Número de componentes')
-plt.ylabel('Variancia explicada cumulativa');
-#cria uma linha horizontal para marcar 0.95
-plt.hlines(y=0.950, xmin = 0, xmax=37, linestyles='dashed', colors='red')
-#Cria uma linha vertical no 15
-plt.vlines(x = 15, ymin = 0.6, ymax=1, linestyles='dashed', colors='red')
-
-
-# In[21]:
-
-
-pca = PCA(0.95).fit(dados_fifa)
-pca.n_components_
-
-
-# In[24]:
-
-
-def q2():
-    return int(pca.n_components_)
-
-
-# In[25]:
-
-
-q2()
-
-
-# ## Questão 3
-# 
-# Qual são as coordenadas (primeiro e segundo componentes principais) do ponto `x` abaixo? O vetor abaixo já está centralizado. Cuidado para __não__ centralizar o vetor novamente (por exemplo, invocando `PCA.transform()` nele). Responda como uma tupla de float arredondados para três casas decimais.
-
-# In[28]:
-
-
-x = [0.87747123,  -1.24990363,  -1.3191255, -36.7341814,
-     -35.55091139, -37.29814417, -28.68671182, -30.90902583,
-     -42.37100061, -32.17082438, -28.86315326, -22.71193348,
-     -38.36945867, -20.61407566, -22.72696734, -25.50360703,
-     2.16339005, -27.96657305, -33.46004736,  -5.08943224,
-     -30.21994603,   3.68803348, -36.10997302, -30.86899058,
-     -22.69827634, -37.95847789, -22.40090313, -30.54859849,
-     -26.64827358, -19.28162344, -34.69783578, -34.6614351,
-     48.38377664,  47.60840355,  45.76793876,  44.61110193,
-     49.28911284
-]
-
+# Discretizando a variável `Pop_density` em 10 intervalos com `KBinsDiscretizer`, seguindo o encode `ordinal` e estratégia `quantile`, quantos países se encontram acima do 90º percentil? Responda como um único escalar inteiro.
 
 # In[29]:
 
 
-pca_fifa = PCA().fit(dados_fifa)
+def q2():
+    # Retorne aqui o resultado da questão 2.
+    # transformando Pop_density em um array do Numpy
+    pop_density = countries_fill['Pop. Density (per sq. mi.)'].to_numpy()
+    pop_density = pop_density.reshape((-1, 1))
+    
+    #KBins & fitting do Sklean
+    kbins = KBinsDiscretizer(n_bins=10, encode='ordinal', strategy = 'quantile')
+    kbins_popd = kbins.fit(pop_density.tolist())
+    
+    #percentil 90
+    percentil_90 = kbins_popd.bin_edges_[0][9]
+    
+    #slice dataset > p90
+    countries_above_p90 = countries_fill[countries_fill['Pop. Density (per sq. mi.)'] > percentil_90]
+    return int(countries_above_p90['Country'].count())
+
+q2()
 
 
-# In[30]:
-
-
-#usando a lista para chamar a função dot do numpy e buscando as duas primeiras posições do PCA
-pca_fifa.components_.dot(x)[0:2]
-
-
-# In[31]:
-
-
-#desempacotando a tupla e arredondando os dados
-comp_1, comp_2 = pca_fifa.components_.dot(x)[0:2].round(3)
-print(comp_1)
-print(comp_2)
-
-
-# In[32]:
-
-
-def q3():
-    return tuple((comp_1, comp_2))
-
+# # Questão 3
+# 
+# Se codificarmos as variáveis `Region` e `Climate` usando _one-hot encoding_, quantos novos atributos seriam criados? Responda como um único escalar.
 
 # In[33]:
 
 
-q3()
+climate = countries_fill[['Climate']].to_numpy().reshape((-1,1))
+region = countries_fill[['Region']].to_numpy().reshape(-1,1)
 
+#Onehotencoder
+label_encoder = OneHotEncoder(categories='auto')
+climate_OHE = label_encoder.fit_transform(climate).toarray()
+region_OHE = label_encoder.fit_transform(region).toarray()
 
-# ## Questão 4
-# 
-# Realiza RFE com estimador de regressão linear para selecionar cinco variáveis, eliminando uma a uma. Quais são as variáveis selecionadas? Responda como uma lista de nomes de variáveis.
-
-# In[34]:
-
-
-#importando do modelo de regressão linear
-from sklearn.linear_model import LinearRegression
+new_columns = climate_OHE.shape[1] + region_OHE.shape[1]
+new_columns
 
 
 # In[35]:
 
 
-#importando biblioteca e utilizando RFE
-from sklearn.feature_selection import RFE
+def q3():
+    # Retorne aqui o resultado da questão 3.
+    return countries['Region'].nunique() + len(countries['Climate'].unique())
+q3()
 
 
-# In[36]:
-
-
-#dividindo o dataset entre variáveis dependentes e variáveis explicativas
-#dependente = y
-y = dados_fifa['Overall']
-y.head()
-
+# ## Questão 4
+# 
+# Aplique o seguinte _pipeline_:
+# 
+# 1. Preencha as variáveis do tipo `int64` e `float64` com suas respectivas medianas.
+# 2. Padronize essas variáveis.
+# 
+# Após aplicado o _pipeline_ descrito acima aos dados (somente nas variáveis dos tipos especificados), aplique o mesmo _pipeline_ (ou `ColumnTransformer`) ao dado abaixo. Qual o valor da variável `Arable` após o _pipeline_? Responda como um único float arredondado para três casas decimais.
 
 # In[37]:
 
 
-#explicativas  = x
-x = dados_fifa.drop(columns = "Overall")
-x.head()
+#Colunas numéricas
+numeric_cols
+
+#Pipeline para os dados numéricos
+#preenchendos dados faltantes com a média
+#Standardization
+
+preprocess = Pipeline(steps=[
+                        ('imput', SimpleImputer(missing_values=np.nan, strategy = 'median')),
+                        ('standard', StandardScaler())
+                        ])
+
+preprocessing_country = preprocess.fit(countries[numeric_cols])
 
 
 # In[38]:
 
 
-#definindo variáveis
-estimator = LinearRegression()
-n_features_to_select = 5
-step = 1
+test_country = [
+    'Test Country', 'NEAR EAST', -0.19032480757326514,
+    -0.3232636124824411, -0.04421734470810142, -0.27528113360605316,
+    0.13255850810281325, -0.8054845935643491, 1.0119784924248225,
+    0.6189182532646624, 1.0074863283776458, 0.20239896852403538,
+    -0.043678728558593366, -0.13929748680369286, 1.3163604645710438,
+    -0.3699637766938669, -0.6149300604558857, -0.854369594993175,
+    0.263445277972641, 0.5712416961268142
+]
 
 
 # In[39]:
 
 
-#Criando o Objeto
-rfe = RFE(estimator, n_features_to_select, step)
+#Criando um dataframe a partir de Countries, testando e reshaping
+test_country = pd.DataFrame(test_country).T
+test_country.columns = countries.columns
+test_country
 
 
 # In[40]:
 
 
-rfe = rfe.fit(x,y)
+def q4():
+    # Retorne aqui o resultado da questão 4.
+    test_processed = preprocessing_country.transform(test_country[numeric_cols])
+    return float (round(test_processed[0,9],3))
 
+q4()
+
+
+# ## Questão 5
+# 
+# Descubra o número de _outliers_ da variável `Net_migration` segundo o método do _boxplot_, ou seja, usando a lógica:
+# 
+# $$x \notin [Q1 - 1.5 \times \text{IQR}, Q3 + 1.5 \times \text{IQR}] \Rightarrow x \text{ é outlier}$$
+# 
+# que se encontram no grupo inferior e no grupo superior.
+# 
+# Você deveria remover da análise as observações consideradas _outliers_ segundo esse método? Responda como uma tupla de três elementos `(outliers_abaixo, outliers_acima, removeria?)` ((int, int, bool)).
 
 # In[41]:
 
 
-rfe.support_
-
-
-# In[42]:
-
-
-#criando um DF que junta o nome das colunas e a seleção dos features
-colunas = pd.DataFrame({'coluna': x.columns,
-                       'bool': rfe.support_})
+country_q1, country_q3 = countries.quantile(q=0.25), countries.quantile(q=0.75)
 
 
 # In[43]:
 
 
-#fazendo query para visualização de colunas
-colunas.query('bool == True')
+def q5():
+    # Retorne aqui o resultado da questão 4.
+    country_q1, country_q3 = countries.quantile(q=0.25), countries.quantile(q=0.75)
+    iqr = country_q3 - country_q1
+    lower, higher = country_q1 - 1.5*iqr, country_q3+1.5*iqr
+    
+    outlier_lower = countries[countries < lower]
+    outlier_higher = countries[countries > higher]
+    
+    result = (
+            int(outlier_lower['Net migration'].dropna().count()),
+            int(outlier_higher['Net migration'].dropna().count()),
+            False)
+    
+    return result
+q5()
 
 
-# In[44]:
+# ## Questão 6
+# Para as questões 6 e 7 utilize a biblioteca `fetch_20newsgroups` de datasets de test do `sklearn`
+# 
+# Considere carregar as seguintes categorias e o dataset `newsgroups`:
+# 
+# ```
+# categories = ['sci.electronics', 'comp.graphics', 'rec.motorcycles']
+# newsgroup = fetch_20newsgroups(subset="train", categories=categories, shuffle=True, random_state=42)
+# ```
+# 
+# 
+# Aplique `CountVectorizer` ao _data set_ `newsgroups` e descubra o número de vezes que a palavra _phone_ aparece no corpus. Responda como um único escalar.
+
+# In[47]:
 
 
-#selecionando os dados do problema
-list(colunas.query('bool==True')['coluna'])
+# dataset
+categories = ['sci.electronics', 'comp.graphics', 'rec.motorcycles']
+newsgroup = fetch_20newsgroups(subset="train", categories=categories, shuffle=True, random_state=42)
 
 
-# In[45]:
+# In[48]:
 
 
-def q4():
-    return list(colunas.query('bool == True')['coluna'])
+def q6():
+    # Retorne aqui o resultado da questão 4.
+    #Word count do Sklearn
+    vectorizer = CountVectorizer()
+    newsgroup_vector = vectorizer.fit_transform(newsgroup['data'])
+    newsgroup_matrix = newsgroup_vector.toarray()
+    
+    #Feature names
+    words_list = vectorizer.get_feature_names()
+    
+    #Chegando se a matriz e a lista de palavras tem a mesma forma
+    newsgroup_matrix.shape[1] == len(words_list)
+    
+    word_count = dict(zip(words_list, newsgroup_matrix.sum(axis=0)))
+    return int(word_count['phone'])
+
+q6()
 
 
-# In[46]:
+# ## Questão 7
+# 
+# Aplique `TfidfVectorizer` ao _data set_ `newsgroups` e descubra o TF-IDF da palavra _phone_. Responda como um único escalar arredondado para três casas decimais.
+
+# In[51]:
 
 
-q4()
+def q7():
+    # Retorne aqui o resultado da questão 4.
+    tf_vector = TfidfVectorizer(use_idf=True)
+    newsgroup_tfvector = tf_vector.fit_transform(newsgroup['data'])
+    
+    tf_names = tf_vector.get_feature_names()
+    tf_array = newsgroup_tfvector.toarray()
+    
+    tf_dict = dict(zip(tf_names, tf_array.sum(axis=0)))
+    
+    return float(round(tf_dict['phone'], 3))
+
+q7()
 
 
 # In[ ]:
